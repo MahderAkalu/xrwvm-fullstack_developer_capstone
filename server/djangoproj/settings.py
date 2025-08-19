421c1ecddefb6c284291b8159ada76fc7107d0e9
MahderAkalu <127959253+MahderAkalu@users.noreply.github.com>
2025-08-19 17:09:38 -0400

    Update 4 settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,18 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    (
-        'https://mahderakalud-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
-    ),
+    'https://mahderakalud-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.' \
+    'proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
     (
-        'https://mahderakalud-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+        'https://mahderakalud-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.' \
         'proxy.cognitiveclass.ai'
     ),
 ]
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
@@ -110,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
     {
-        'NAME': 
+        'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
