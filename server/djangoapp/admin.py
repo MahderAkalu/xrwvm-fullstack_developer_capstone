303389ff0ae66a431978ad250159008d54406a71
MahderAkalu <127959253+MahderAkalu@users.noreply.github.com>
2025-08-19 14:28:19 -0400

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -10,7 +10,12 @@ class CarModelInline(admin.TabularInline):  # shows CarModels inside CarMake
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ("name", "car_make", "type", "year")   # use 'type' not 'car_type'
+    list_display = [
+    "name",
+    "car_make",
+    "type",
+    "year"
+]   # use 'type' not 'car_type'
     list_filter = ("type", "year", "car_make")            # same here
     search_fields = ("name",)
 
