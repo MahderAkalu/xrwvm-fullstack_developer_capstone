73422a89d34e9a7f61487063938174460f9ce2f1
MahderAkalu <127959253+MahderAkalu@users.noreply.github.com>
2025-08-19 16:49:32 -0400

    Update 3 restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -24,7 +24,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except requests.exceptions.RequestException as e::
+    except requests.exceptions.RequestException as e:
         # If any error occurs
         print("Network exception occurred")
 
